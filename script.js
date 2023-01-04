3b82c8aa1221359f6988c268e0f407022ee3e521
thatoneguy087 <adeptpenguin08@gmail.com>
2023-01-03 21:09:08 -0700

    Add "rock" case to new function playRound

--- script.js
+++ script.js
@@ -18,12 +18,22 @@ function getComputerChoice() {
 }
   //get user input and store it in a variable
 const playerSelection = prompt('What is your move?').toLowerCase();
-console.log(playerSelection);
 //create function to play one round of rock paper scissors
 
   //compare user input agains't computer for all possibilities
   //return result.
-
+function playRound(playerSelection, computerSelection) {
+  if (computerSelection === 'rock') {
+    if (playerSelection === 'paper') {
+      return 'You win! Paper beats rock!';
+    } else if (playerSelection == 'scissors') {
+      return 'You lose! Rock beats scissors!';
+    } else {
+      return "It's a tie!";
+    }
+  }
+}
+console.log(playRound(playerSelection, 'rock'));
 //create function that plays five rounds
   //loop round function five times.
   //output final result
